05c056c1eba7bc1daf0a16daa418ecb73bdb2af1
Michael Wells <mwells1286@gmail.com>
2020-04-23 14:02:13 -0700

    update output text and clean file.

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 require('fileutils')
 require('bundler')
+
 File.write('bad-gems.txt', "")
 puts File.read('art.txt')
 fileToScan = File.read('fileToScan')
 maliciousGems = File.readlines('ruby_malicious_gems.txt')
 maliciousArray=[]
+
 maliciousGems.each do |line|
   line = line.split(' ')
   maliciousArray.push(line[0])
@@ -15,9 +17,11 @@ maliciousArray.each do |malGem|
      `grep #{malGem} ./fileToScan >> bad-gems.txt`   
    end
 end
- badGems = File.readlines('bad-gems.txt')
+
+badGems = File.readlines('bad-gems.txt')
+
 if badGems.length > 0
-  puts "We found " + badGems.length.to_s + " vulnerable gems!"
+  puts "We found " + badGems.length.to_s + " potentially vulnerable gems! Check version numbers to see if your version has been patched."
   puts badGems
   puts "find a log in bad-gems.txt in this directory" 
 else
