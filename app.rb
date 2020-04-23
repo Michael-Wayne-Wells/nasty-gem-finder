9bc939969005c92f1d5e5734e30e14b4a80f09dc
Michael Wells <mwells1286@gmail.com>
2020-04-23 12:27:05 -0700

    update app to return versions and utilize grep

--- app.rb
+++ app.rb
@@ -1,22 +1,22 @@
 require('fileutils')
 require('bundler')
-
-puts "Find those"
+File.write('bad-gems.txt', "")
 puts File.read('art.txt')
 fileToScan = File.read('fileToScan')
 maliciousGems = File.readlines('ruby_malicious_gems.txt')
 maliciousArray=[]
-badGems = []
 maliciousGems.each do |line|
   line = line.split(' ')
   maliciousArray.push(line[0])
 end
 
 maliciousArray.each do |malGem|
-  badGems.push(malGem) if fileToScan =~ /#{malGem}/i 
+   if fileToScan =~ /\b#{malGem}\b/i
+     `grep #{malGem} ./fileToScan >> bad-gems.txt`   
+   end
 end
+ badGems = File.readlines('bad-gems.txt')
 if badGems.length > 0
-  File.write("bad-gems.txt", badGems)
   puts "We found " + badGems.length.to_s + " vulnerable gems!"
   puts badGems
   puts "find a log in bad-gems.txt in this directory" 
